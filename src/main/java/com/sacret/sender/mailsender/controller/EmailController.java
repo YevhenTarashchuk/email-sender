89883aef35d0bd9dcd8d0d9c1375c5f28d05bb6c
YevhenTarashchuk <yevhen.tarashchuk@coreteka.com>
2022-03-24 16:58:40 +0200

    feat(EmailController): update sendEmails

--- src/main/java/com/sacret/sender/mailsender/controller/EmailController.java
+++ src/main/java/com/sacret/sender/mailsender/controller/EmailController.java
@@ -3,7 +3,7 @@ package com.sacret.sender.mailsender.controller;
 import com.sacret.sender.mailsender.model.dto.JobRequestDTO;
 import com.sacret.sender.mailsender.model.dto.JobResponseDTO;
 import com.sacret.sender.mailsender.model.response.CustomResponse;
-import com.sacret.sender.mailsender.service.EmailSenderService;
+import com.sacret.sender.mailsender.service.EmailJobService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -20,12 +20,12 @@ import javax.validation.Valid;
 @RequestMapping("/emails")
 public class EmailController {
 
-    EmailSenderService emailSenderService;
+    EmailJobService emailJobService;
 
     @PostMapping
     public CustomResponse<JobResponseDTO> sendEmails(@RequestBody @Valid JobRequestDTO dto) {
         return new CustomResponse<JobResponseDTO>()
-                .setData(emailSenderService.startEmailJob(dto))
+                .setData(emailJobService.startEmailJob(dto))
                 .setStatus(CustomResponse.Status.OK);
     }
 }
