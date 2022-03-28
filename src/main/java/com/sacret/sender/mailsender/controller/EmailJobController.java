0aa127ac1dd78cccc0dec71caf25e814f7ffb090
YevhenTarashchuk <yevhen.tarashchuk@coreteka.com>
2022-03-28 21:43:59 +0300

    refactor(EmailJobController): rename controller

--- src/main/java/com/sacret/sender/mailsender/controller/EmailJobController.java
+++ src/main/java/com/sacret/sender/mailsender/controller/EmailJobController.java
@@ -7,18 +7,15 @@ import com.sacret.sender.mailsender.service.EmailJobService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
-@RequestMapping("/emails")
-public class EmailController {
+@RequestMapping("/job")
+public class EmailJobController {
 
     EmailJobService emailJobService;
 
