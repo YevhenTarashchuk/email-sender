e2d4296dd2f79290bca1f0337f7cb06d0742efd7
YevhenTarashchuk <yevhen.tarashchuk@coreteka.com>
2022-03-28 23:26:33 +0300

    feat(JobStatusService): add getJobStatus

--- src/main/java/com/sacret/sender/mailsender/service/JobStatusService.java
+++ src/main/java/com/sacret/sender/mailsender/service/JobStatusService.java
@@ -1,12 +1,15 @@
 package com.sacret.sender.mailsender.service;
 
+import com.sacret.sender.mailsender.exception.BaseException;
 import com.sacret.sender.mailsender.model.entity.EmailJob;
+import com.sacret.sender.mailsender.model.enumaration.ErrorCode;
 import com.sacret.sender.mailsender.model.enumaration.JobStatus;
 import com.sacret.sender.mailsender.repository.EmailJobRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,5 +29,12 @@ public class JobStatusService {
 
         LOG.info("EmailJobId: {} has status: {}", job.getId(), job.getJobStatus().toString());
         return job;
+    }
+
+    public String getJobStatus(String jobId) {
+        return emailJobRepository.findById(jobId).orElseThrow(() -> {
+           LOG.warn("JobId: {} is not found", jobId);
+           return new BaseException(HttpStatus.BAD_REQUEST, ErrorCode.ERR_JOB_NOT_FOUND);
+       }).getJobStatus().toString();
     }
 }
